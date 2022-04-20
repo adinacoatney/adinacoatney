97c71c938cbce0129eb236f125fd32b827adc42b
Adina Coatney <adinacoatney@hotmail.com>
2022-04-19 20:33:48 -0500

    Back to Top Added / About Link Fixed

--- script.js
+++ script.js
@@ -19,3 +19,22 @@ function toggleLight(){
     document.body.style.color = "#533B34"
 }
 light.addEventListener('click', toggleLight)
+
+
+topArrow= document.getElementById("topArrow");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      topArrow.style.display = "block";
+    } else {
+      topArrow.style.display = "none";
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
